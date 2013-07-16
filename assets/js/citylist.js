22aad20c2e0089ef6b7ba5b60be744ae3701d4c0
Ankit <ankit@ankit-Veriton-Series.(none)>
2013-07-16 16:25:24 +0530

    jh

---
+++ assets/js/citylist.js
@@ -0,0 +1,39 @@
+ var citiesByState = {
+            AN: ["North Andaman","South Andaman","Nicobar"],
+			
+            AP: ["Adilabad","Anantapur", "Chittoor", "East Godavari", "Guntur", "Hyderabad", "Karimnagar", "Khammam", "Krishna", " Kurnool", "Mahbubnagar", "Medak", "Nalgonda", "Nizamabad", "Prakasam", "Ranga Reddy", "Srikakulam", "Sri Potti Sri Ramulu Nellore", "Vishakhapatnam", "Vizianagaram", "Warangal", "West Godavari", " 	YSR district",],
+			
+			AR: [" Anjaw", "Anjaw", "Changlan", "East Siang", "East Kameng", "Kurung Kumey", "Lohit", "Lower Dibang Valley", "Lower Subansiri", "Papum Pare", "Tawang", "Tirap", "Dibang Valley", "Upper Siang", "Upper Subansiri", "West Kameng", "West Siang"],
+			
+			AS: ["Baksa", "Barpeta", "Bongaigaon", "Cachar", "Chirang", "Darrang", "Dhemaji", "Dima", "Hasao", "Dhubri", "Dhibrugarh", "Goalpara", "Golaghat", "Hailakandi", "Jorgat", "Kamrup", "Kamrup Metropolitan", "Karbi", "Anglong", "Karimganj", "Kokrajhar", "Lakhimpur", "Marigaon", "Nagaon", "Nalbari", "Sibsagar", "Sonitpur", "Tinsukia", "Udalguri"],
+			
+			BR: ["Araria", "Arwal", "Aurangabad", "Banka", "Begusarai", "Bhagalpur", "Bhojpur", "Buxar", "Darbhanga", "East Champaran", "Gaya",
+"Gopalpanj", "Jamui", "Jehanabad", "Kaimur", "Katihar", "Khagaria", "Kishanganj", "Lakhisarai", "Madhepura", "Madhubani", "Munger", "Muzaffarpur", "Nalanda", "Nawada", "Patna", "Purnia", "Rohtas", "Saharsa", "Samastipur", "Saran", "Sheikhpura", "Sheohar", "Sitamarhi", "Siwan", "Supaul", "Vaishali", "West Champaran"],
+			
+			CH: ["Chandigarh"],
+			
+			CG: ["Bastar", "Bijapur", "Bilaspur", "Dantewada", "Dhamtari", "Durg", "Jashpur", "Jangir-Champa", "Korba", "koriya", "Kanker", "Kabirdham", "Mahasamund", "Narayanpur", "Raigarh", "Rajnandgaon", "Raipur", "Surguja"],
+			
+			DN: ["Dadra and Nagar Haveli"],
+			
+			DD: ["Daman", "Diu"],
+			
+			DL: ["Central Delhi", "East Delhi", "New Delhi", "North Delhi", "North East Delhi", "North West Delhi", "South West Delhi", "West Delhi"],
+			
+			GA: ["North Goa", "South Goa"],
+			
+			GJ: ["Ahmedabad", "Amreli district", "Anand", "Banaskantha", "Bharuch", "Bhavnagar", "Dahod", "The Dangs", "Gandhinagar", "Jamnagar", "Junagarh", "Kutch", "Kheda", "Mehsana", "Narmada", "Navsari", "Patan", "Panchmal", "Porbandar", "Rajkot", "Sabarkantha", "Surendranagar", "Surat", "Tapi", "Vadodara", "Valsad"],
+			
+			HR: ["Ambala", "Bhiwani", "Faridabad", "Fatehabad", "Gurgaon", "Hissar", "Jhajjar", "Jind", "Karnal", "Kaithal", "Kurukshetra", "Mahendragarh", "Mewat", "Palwal", "Panchkula", "Panipat", "Rewari", "Rohtak", "Sirsa", "Sonipat", "Yamuna Nagar"],
+			
+            HP: ["Bilaspur", "Chamba", "Hamirpur", "Kangra", "Kinnaur", "Kullu", "Lahaul and Spiti", "Mandi", "Shimla", "Sirmaur", "Solan", "Una"],
+			
+			JK: ["Anantnag", "Badgam", "Bandipora", "Baramulla", "Doda", "Ganderbal", "Jammu", "Kargil", "Kathua", "Kishtwar", "Kupwara", "Kulgam", "Leh", "Poonch", "Pulwana", "Rajauri", "Ramban", "Reasi", "Samba", "Shopian", "Srinagar", "Udhampur"],
+			
+			JH: ["Bokaro", "Chatra", "Deoghar", "Dhanbad", "Dumka", "East Singhbhum", "Garhwa", "Giridih", "Godda", "Gumla", "Hazaribag", "Jamtara", "Khunti", 
+"Koderma", "Latehar", "Lohardaga", "Pakur", "Palamu", "Ramgarh", "Ranchi", "Sahibganj", "Seraikela Kharsawan", "Simdega", "West Singhbhum" ],
+			
+			KA: ["Bagalkot", "Bangalore Rural", "Bangalore Urban", "Belgaum", "Bellary", "Bidar", "Bijapur", "Chamarajnagar", "Chikkamagaluru", "Chikkaballapur", "Chitradurga", "Davanagere", "Dharwad", "Dakshina Kannada", "Gadag", "Gulbarga", "Hassan", "Haveri District", "Kodagu", "Kolar", "Koppal", "Mandya", "Mysore", 
+"Raichur", "Shimoga", "Tumkur", "Udupi", "Uttara Kannada", "Ramanagara", "Yadgir"]
+			}
+		
